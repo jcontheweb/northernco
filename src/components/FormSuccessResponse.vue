41088a280a526dd246b21f96b1a15c1ffd4b4f01
jcontheweb <cairns.webdev@gmail.com>
2020-12-13 20:10:16 +0000

    responsive breakpoints

--- src/components/FormSuccessResponse.vue
+++ src/components/FormSuccessResponse.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h3 class="font-bold text-3xl text-white font-sans">Thanks for your interest!</h3>
+    <h3 class="font-bold text-xl md:text-3xl text-white font-sans">Thanks for your interest!</h3>
     <p
-      class="text-xl text-white mt-6"
+      class="md:text-xl text-white mt-6"
     >We will review your application and contact you for additional information should your background and experience meet the requirements of one of our openings.</p>
   </div>
 </template>
